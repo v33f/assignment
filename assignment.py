2cafa203e2dba7dc37c292b49ab642ee4a147acd
v33f <95421694+v33f@users.noreply.github.com>
2021-12-02 16:59:44 +0400

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,12 @@
+Python 3.9.9 (tags/v3.9.9:ccb0e6a, Nov 15 2021, 18:08:50) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> r = float(input ("Input the radius of the circle : "))
+Input the radius of the circle : 1.1
+>>> from math import pi
+>>> print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+The area of the circle with radius 1.1 is: 3.8013271108436504
+>>> filename = input("Input the Filename: abc.py")
+Input the Filename: abc.py
+>>> print ("The extension of the file is : python")
+The extension of the file is : python
+>>> 
