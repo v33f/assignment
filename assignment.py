62be487d1bc5f977105cd0b9c5d9bd2e8a3ab8d4
v33f <95421694+v33f@users.noreply.github.com>
2021-12-03 21:19:10 +0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,5 +1,3 @@
-Python 3.9.9 (tags/v3.9.9:ccb0e6a, Nov 15 2021, 18:08:50) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> r = float(input ("Input the radius of the circle : "))
 Input the radius of the circle : 1.1
 >>> from math import pi
@@ -9,4 +7,3 @@ The area of the circle with radius 1.1 is: 3.8013271108436504
 Input the Filename: abc.py
 >>> print ("The extension of the file is : python")
 The extension of the file is : python
->>> 
